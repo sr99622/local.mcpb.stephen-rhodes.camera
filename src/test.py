6d362d29a5005b8927b119c16938157edac72a7e
Stephen Rhodes <sr99622@gmail.com>
2026-07-10 17:25:51 -0400

    initial commit

---
+++ src/test.py
@@ -0,0 +1,58 @@
+import os
+import sys
+import logging
+from libonvif.devices.camera import Camera, discover, get_camera_by_ip, set_hostname
+from libonvif.utils.adapters import find_adapters
+import niquests as requests
+from urllib.parse import urlparse
+
+from niquests.auth import HTTPBasicAuth, HTTPDigestAuth
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+def get_camera_credentials(camera: Camera) -> None:
+    camera.username = os.environ.get("CAMERA_USERNAME", "")
+    camera.password = os.environ.get("CAMERA_PASSWORD", "")
+
+def on_error(xaddr: str, ex: Exception) -> None:
+    logger.debug(f"error: {xaddr} - {ex}")
+
+def camera_filled(camera: Camera) -> None:
+    logger.debug(f"Camera Filled: {camera.hostname.name} : {camera.device_information.serial_number}")
+
+def main():
+    logger.debug("Starting test...")
+    adapters = find_adapters()
+    for adapter in adapters:
+        logger.debug(f"Found adapter: {adapter}")
+
+    ip_address = adapters[0] if adapters else "0.0.0.0"
+    cameras = discover(ip_address,
+                       get_camera_credentials,
+                       on_error=on_error,
+                       camera_filled=camera_filled,
+                       use_threads=True)
+    
+    for camera in cameras:
+        uri = camera.profiles[0].snapshot_uri if camera.profiles else "No profiles available"
+        logger.debug(f"Discovered camera: {camera.hostname.name} - {uri}")
+
+        if camera.hostname.name:
+            logger.debug(f"Found camera {camera.hostname.name}")
+            parsed = urlparse(uri)
+            simple_url = f"{parsed.scheme}://{camera.username}:{camera.password}@{parsed.netloc}{parsed.path}?{parsed.query}"
+            logger.debug(f"Simple URL: {simple_url}")
+            response = requests.get(uri, auth=HTTPDigestAuth(camera.username, camera.password), timeout=5)
+            logger.debug(f"Response status code: {response.status_code}")
+            if response.status_code == 200:
+                logger.debug("Successfully accessed the camera snapshot.")
+                with open(f"snapshot_{camera.hostname.name}.jpg", "wb") as f:
+                    f.write(response.content)
+                    logger.debug(f"Snapshot saved to snapshot_{camera.hostname.name}.jpg")
+            else:
+                logger.error(f"Failed to access the camera snapshot. Status code: {response.status_code}")
+
+
+if __name__ == "__main__":
+    main()
